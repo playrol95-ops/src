a8834c7d162eca093325e75ab2413790efb89ce8
paola <you@example.com>
2025-12-30 12:17:28 -0400

    commit inicial

---
+++ algo/saludo.py
@@ -0,0 +1,7 @@
+nombre = "paola"
+if nombre == "gemini":
+    print("Hola, soy tu asistente")
+    print("estoy dentro del bloque if gracias a la identacion")
+    else:
+    print("No se quien eres")
+    print("yo estoy fuera de todo, asi que siempre me veras")
